2a674676118a51325a1fe3fd764b3a480938dde8
ilhamarisanto <ilhamarisanto@gmail.com>
2019-04-21 23:55:47 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,340 @@
+<!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Daikin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" media="screen" href="style/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style/css/style.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/solid.css" integrity="sha384-r/k8YTFqmlOaqRkZuSiE9trsrDXkh07mRaoGBMoDcmA58OHILZPsk29i2BsFng1B" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/regular.css" integrity="sha384-IG162Tfx2WTn//TRUi9ahZHsz47lNKzYOp0b6Vv8qltVlPkub2yj9TVwzNck6GEF" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/brands.css" integrity="sha384-BKw0P+CQz9xmby+uplDwp82Py8x1xtYPK3ORn/ZSoe6Dk3ETP59WCDnX+fI1XCKK" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/fontawesome.css" integrity="sha384-4aon80D8rXCGx9ayDt85LbyUHeMWd3UiBaWliBlJ53yzm9hqN21A+o1pqoyK04h+" crossorigin="anonymous">
+    <!--Javascript-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 </head>
-
-
-<h1> Hello World </h1>
-
 <body>
+    <div class='header'>
+        <div class='container-fluid'>
+            <div class='row'>
+                <div class='col-ms-6 col-md-6 col-lg-6'>
+                    <div class='logo'>
+                        <img src="image/logo.png" alt="Logo Daikin"/>
+                    </div>
+                </div>
+
+                <div class='col-ms-6 col-md-6 col-lg-6'>
+                    <div class='logo1'>
+                        <img src="image/logo_daikin.png" alt="Logo Daikin"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light nav">
+        <div class='container'>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ABOUT US
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Drop 1</a>
+                            <a class="dropdown-item" href="#">Drop 2</a>
+                            <a class="dropdown-item" href="#">Drop 3</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PRODUCTS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">SERVICES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href='#'>CUSTOMERS</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href='#'>GALERY</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href='#'>CAREER</a>
+                    </li>
+                    <li calss="nav-item">
+                    <div class="searchbar">
+                    <input class="search_input" type="text" name="" placeholder="Search...">
+                        <i class="fas fa-search"></i>
+                    </div>
+                    </li> 
+                </ul>
+            </div>
+        </div>
+        
+</nav>
+
+        <div class='slider'>
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                   
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img class="d-block w-100" src="image/Slide Image.jpg" alt="First slide">
+                    </div>
+                    
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+
+        <div class='container'>
+           <div class='row'>
+                <div class='col-ms-12 col-md-12 col-lg-12'>
+                    <div class='judul'>
+                        <h4>PRODUCTS & SERVICES</h4>
+                    </div> 
+                </div>
+            </div>
+               <div class='container_circle'>
+               <div class='row'>
+                    <a href='#'>
+                    <div class='col-ms-3 col-md-3 col-lg-3'>
+                        <div class="circle_snow">
+                            <h2 class='h2_snow'>Product</h2>
+                            <i class="fas fa-snowflake" id = 'i'> </i>
+                        </div>
+                    </div>
+                    </a>
+                    <a href='#'>
+                    <div class='col-ms-3 col-md-3 col-lg-3'>
+                        <div class="circle_ins">
+                            <h2 class='h2_ins'>Installation</h2>
+                             <i class="fas fa-tools" id = 'i'> </i>
+                        </div>
+                    </div>
+                    </a>
+                    <a href='#'>
+                    <div class='col-ms-3 col-md-3 col-lg-3'>
+                         <div class="circle_main">
+                            <h2 class='h2_main'>Maintenance</h2>
+                            <i class="fas fa-cog" id = 'i'></i>
+                        </div>
+                    </div>
+                    </a>
+                    <a href='#'>
+                    <div class='col-ms-3 col-md-3 col-lg-3'>
+                         <div class="circle_re">
+                            <h2 class='h2_re'>Repair</h2>
+                            <i class="fas fa-user-cog" id = 'i'></i>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class='row'>
+                <div class='col-ms-12 col-md-12 col-lg-12'>
+                    <div class='judul'>
+                        <h4>OFFERS</h4>
+                    </div> 
+                </div>
+            </div>
+
+            <div class='row'>
+                <div class='slider'>
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img class="d-block w-100" src="image/Banner Offers.jpg" alt="First slide">
+                            </div>
+
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class='row'>
+                <div class='col-ms-7 col-md-7 col-lg-7'>
+                    <div class='judul'>
+                        <h4>NEWS & ARTICLE</h4>
+                    </div> 
+                     <div class='row'>
+                        <div class='col-ms-7 col-md-7 col-lg-7'>
+                            <div class='news'>
+                                <a href='#'>
+                                <img src='image/living.jpg'>
+                                <div class='judul_detail'>
+                                Catat ini, 3 Manfaat 
+                            Menggunakan AC Daikin Multi S 
+                                </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class='col-ms-5 col-md-5 col-lg-5'>
+                            <div class='col-ms-12 col-md-12 col-lg-12'>
+                                <div class='news_detail'>
+                                 <a href='#'>
+                                    <img src='image/tips untuk suhu ideal ruangan.jpg'>
+                                    <div class='judul_detail'>
+                                    Tips untuk Suhu 
+                                    ideal Ruangan 
+                                    </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class='col-ms-12 col-md-12 col-lg-12'>
+                                <div class='news_detail'>
+                                <a href='#'>
+                                    <img src='image/kurangi efek pemanasan global.jpg'>
+                                    <div class='judul_detail'>
+                                    Kurangi efek
+                                    pemanasan global 
+                                    </div>
+                                   </a> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class='col-ms-5 col-md-5 col-lg-5'>
+                    <div class='judul'>
+                        <h4>LOCATION</h4>
+                    </div> 
+                    
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.059775773903!2d107.60404401407958!3d-6.883443195025941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6f6fe84fb1f%3A0xbc4b4af35420809e!2sSarana+Tunggal+Mulia.+PT!5e0!3m2!1sid!2sid!4v1553014995213" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+					
+					
+					
+                </div>
+            </div>
+        </div>
+
+        <div class='footer'>
+            <div class='container'>
+                <div class='row'>
+                    
+                    <div class='col-ms-4 col-md-4 col-lg-4'>
+                        <h4>CUSTOMER SERVICE</H4>
+                        <div class='row'>
+                            <div class='col-ms-3 col-md-3 col-lg-3'>
+                                 <i class="fas fa-phone-square footer_cus"></i>
+                            </div>
+                            <div class='col-ms-6 col-md-6 col-lg-6'>
+                                <p>
+                                    Office<br/>
+                                    (022) 2031865<br/>
+                                    
+                                    Jl. Siliwangi no. 14-e,
+                                        Bandung, 40135
+                                </p>
+                            </div>
+                            <div class='col-ms-12 col-md-12 col-lg-12'>
+                                
+                                <p>
+                                    <i class="far fa-envelope"></i> info@saranatunggalmulia.com
+                                </p>
+                            </div>                
+                       
+                        </div>
+                    </div>
+                    
+                    <div class='col-ms-4 col-md-4 col-lg-4'>
+                        <div class='row'>
+                            <div class='col-ms-12 col-md-12 col-lg-12'>
+                            <h4>CONNECT WITH US</H4>
+                                    <a href='#'><i class="fab fa-instagram footeri"></i></a>
+                                    <a href='#'><i class="fab fa-facebook footeri"></i></a>
+                                    <a href='#'><i class="fab fa-youtube footeri"></i></a>
+                            </div>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <div class='col-ms-12 col-md-12 col-lg-12'>
+                            <h4>PRE-ORDER</H4>
+                                <div class='row'>
+
+                                    <div class='col-ms-8 col-md-8 col-lg-8'>
+                                        <div class="form-group">
+                                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" style="width:'100%'">
+                                        </div>
+                                    </div>
+                                    <div class='col-ms-2 col-md-2 col-lg-2'>
+                                        <div class="form-group">
+                                            <button type="button" class="btn btn-light">GO</button>
+                                        </div>
+                                    </div>
+                                </div>
+                           
+                            </div>                
+                       
+                        </div>
+                    </div>
+
+                    <div class='col-ms-4 col-md-4 col-lg-4'>
+                        <div class='row'>
+                            <div class='col-ms-6 col-md-6 col-lg-6'>
+                                <div class='mascot'>
+                                    <img src='image/B-Pinchonkun.png'/>
+                                </div>
+                            </div>
+
+                            <div class='col-ms-6 col-md-6 col-lg-6'>
+                                <div class='mascot1'>
+                                    <img src='image/P-Pinchonkun.png'/>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    </div>
+            </div>
+        </div>
+                  
+
+        <div class='copyright'>
+            <div class='container'>
+                <div class='row'>
+                    <div class='col-ms-6 col-md-6 col-lg-6'>
+                    <a href='#'>Sitemap</a> &nbsp;&nbsp;&nbsp;  <a href='#'>Disclaimer</a>
+                    </div>
+                    <div class='col-ms-6 col-md-6 col-lg-6'>
+                        <div class='right'>&copy; Copyright 2019. PT Sarana Tunggal Mulia. All right reserved</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 </body>
 </html>
